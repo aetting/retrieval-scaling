3eccb86ecdfff2a7b6e291c004e6c5e1d663b7b4
Allyson Ettinger <allysone@allenai.org>
2024-12-11 14:54:38 -0800

    add main scripts

---
+++ run_embedding.sh
@@ -0,0 +1,16 @@
+# PYTHONPATH=. python3.11 src/embed.py --model_name_or_path contriever
+
+# PYTHONPATH=.  python ric/main_ric.py --config-name example_config
+
+# BEAKER_REPLICA_RANK=1 BEAKER_REPLICA_COUNT=2 python -m ric.main_ric --config-name example_config
+
+python -m ric.main_ric --config-name example_config
+
+# export PYTHONPATH=.
+
+# torchrun \
+#     --nnodes=1 \
+#     --nproc-per-node=4 \
+#     -m ric.main_ric --config-name example_config 
+
+
